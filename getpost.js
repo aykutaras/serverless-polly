1ba98fbe4125f7a804b6b322228ddee84102ae01
Aykut Aras <aykutaras@gmail.com>
2018-02-05 17:11:30 +0300

    Added serverless roles and buckets

--- getpost.js
+++ getpost.js
@@ -1,56 +1,41 @@
+import AWS from "aws-sdk";
+
 // eslint-disable-next-line import/prefer-default-export
-export const hello = (event, context, callback) => {
-  const p = new Promise((resolve) => {
-    resolve('success');
-  });
+export const main = async (event, context, callback) => {
+    const docClient = new AWS.DynamoDB.DocumentClient();
+    const postId = event.postId;
+    const params = {
+        TableName: "aaras-polly"
+    };
 
-  p.then(() => callback(null, {
-      message: 'Go Serverless Webpack (Ecma Script) v1.0! First module!',
-      event,
-    }))
-    .catch(e => callback(e));
+    if (postId !== '*') {
+        params.Key = {
+            "id": postId
+        };
+        
+        let data = await docClient.get(params).promise();
+        var response = {
+            "statusCode": 200,
+            "body": [data.Item],
+            "isBase64Encoded": false
+        };
+
+        callback(null, response);
+        //    if (err) {
+        //        callback("Unable to read item. Error JSON:" + JSON.stringify(err, null, 2), null);
+        //    } else {
+        //    }
+    } else {
+        let scanData = await docClient.scan(params).promise();
+        var response = {
+            "statusCode": 200,
+            "body": scanData.Items,
+            "isBase64Encoded": false
+        };
+
+        callback(null, response);
+         //   if (err) {
+         //       callback("Unable to read item. Error JSON:" + JSON.stringify(err, null, 2), null);
+         //   }
+    }
 };
-
-//const AWS = require("aws-sdk");
-//
-//exports.handler = (event, context, callback) => {
-//    const docClient = new AWS.DynamoDB.DocumentClient();
-//    const postId = event.postId;
-//    const params = {
-//        TableName: "aaras-polly"
-//    };
-//
-//    if (postId !== '*') {
-//        params.Key = {
-//            "id": postId
-//        };
-//        
-//        docClient.get(params, function(err, data) {
-//            if (err) {
-//                callback("Unable to read item. Error JSON:" + JSON.stringify(err, null, 2), null);
-//            } else {
-//                var response = {
-//                    "statusCode": 200,
-//                    "body": [data.Item],
-//                    "isBase64Encoded": false
-//                };
-//                
-//                callback(null, response);
-//            }
-//        });
-//    } else {
-//        docClient.scan(params, function(err, data) {
-//            if (err) {
-//                callback("Unable to read item. Error JSON:" + JSON.stringify(err, null, 2), null);
-//            } else {
-//                var response = {
-//                    "statusCode": 200,
-//                    "body": data.Items,
-//                    "isBase64Encoded": false
-//                };
-//                
-//                callback(null, response);
-//            }
-//        });
-//    }
-//};
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
 const path = require('path');
 // eslint-disable-next-line import/no-unresolved
 const slsw = require('serverless-webpack');
+const nodeExternals = require("webpack-node-externals");
 
 module.exports = {
   entry: slsw.lib.entries,
   target: 'node',
+  externals: [nodeExternals()],
   module: {
     loaders: [{
       test: /\.js$/,
